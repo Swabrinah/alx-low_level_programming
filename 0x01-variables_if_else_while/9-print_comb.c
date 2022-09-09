252df4c95b544d08d76558c192790c0f44bb65ff
Swabrinah <zubedaraya@gmail.com>
2022-09-09 10:35:18 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ * main - The main function is the entry point
+ *
+ * The programs prints combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	int d;
+
+
+
+	for (d = '0'; d <= '9'; d++)
+
+	{
+
+		putchar(d);
+
+		if (d != '9')
+
+		{
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
+
+Footer
+
+© 2022 Git
