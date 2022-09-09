df0623b5b37fc315d3fc83700359f8d976383705
Swabrinah <zubedaraya@gmail.com>
2022-09-09 09:05:15 -0700

    base 20

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ * main - the main function is the entry point
+ *
+ * The program prints numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	int d;
+
+
+
+	for (d = '0'; d <= '9'; d++)
+
+		putchar(d);
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
