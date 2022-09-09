9c421981a840f4500a6718d9450491036873f9bd
Swabrinah <zubedaraya@gmail.com>
2022-09-09 09:01:45 -0700

    single

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ * main - The main function is the entry point
+ *
+ * The progam prints single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	int i;
+
+
+	for (i = 0; i < 10; i++)
+
+	 printf("%d", i);
+
+	 printf("\n");
+
+
+	return (0);
+
+}
